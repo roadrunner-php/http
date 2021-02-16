59a2c16f8b72eac021ca3688074a127348f3c916
SerafimArts <nesk@xakep.ru>
2021-02-17 02:31:24 +0300

    Actualize docblock

--- src/PSR7WorkerInterface.php
+++ src/PSR7WorkerInterface.php
@@ -1,7 +1,10 @@
 <?php
 
 /**
- * High-performance PHP process supervisor and load balancer written in Go. Http core.
+ * This file is part of RoadRunner package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 declare(strict_types=1);
