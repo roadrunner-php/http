fddab460b4a4ab7028bb5df61db3dae34bcfed12
blezigen <khairullin.it@gmail.com>
2025-11-26 11:30:41 +0300

    fix(worker): Fix wrong autodetect json body
    Change wrong detect codec from header.
    Change header to body.

--- src/HttpWorker.php
+++ src/HttpWorker.php
@@ -64,7 +64,7 @@ class HttpWorker implements HttpWorkerInterface
         }
 
         if (static::$codec === null) {
-            static::$codec = \json_validate($payload->header) ? Frame::CODEC_JSON : Frame::CODEC_PROTO;
+            static::$codec = \json_validate($payload->body) ? Frame::CODEC_JSON : Frame::CODEC_PROTO;
         }
 
         if (static::$codec === Frame::CODEC_PROTO) {
